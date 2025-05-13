482275d6e61ef0393fec1b6d9637b535bd54e94e
Vitaliy Bashkir <vabashkir@icloud.com>
2025-05-13 10:31:04 +0300

    js and fix bug styles

---
+++ js/modal.js
@@ -0,0 +1,19 @@
+(() => {
+    const refs = {
+      // Добавить атрибуты data-modal-open на кнопку открытия
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      // Добавить атрибуты data-modal-close на кнопку закрытия
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      // Добавляем атрибуты data-modal на бекдроп модалки
+      modal: document.querySelector('[data-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      // Открыть этот класс, который нужно будет добавить/удалить на бэкдропе при нажатии на кнопку
+      refs.modal.classList.toggle('is-open');
+    }
+  })();
+  
